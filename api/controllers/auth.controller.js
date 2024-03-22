70101e7813ae045bbc9cd956b2d2647107f1c632
nihal <nihalmmuneer@gmail.com>
2024-03-22 11:33:29 +0530

    google auth creation on google button

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -69,3 +69,56 @@ export const signin = async (req, res, next) => {
     return next(error);
   }
 };
+export const google = async (req, res, next) => {
+  const { username, email, googlePhotoUrl } = req.body;
+  try {
+    const user = await User.findOne({
+      email,
+    });
+    if (user) {
+      const token = jwt.sign(
+        {
+          id: user._id,
+        },  
+        process.env.SECRET_KEY
+      );
+      const { password, ...rest } = user._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
+    } else {
+      const generatePassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+
+      const hashedpassword = bcryptjs.hashSync(generatePassword, 10);
+      const newUser = new User({
+        username:
+          username.toLowerCase().split(' ').join('') +
+          Math.random().toString(9).slice(-4),
+        email,
+        password: hashedpassword,
+        profilePicture: googlePhotoUrl,
+      });
+      await newUser.save();
+      const token = jwt.sign(
+        {
+          id: newUser._id,
+        },
+        process.env.SECRET_KEY
+      );
+      const { password, ...rest } = newUser._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,23 +2,30 @@
 import mongoose from "mongoose";
 
 // Providing a set of rules
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profilePicture: {
+        type: String,
+        default:"https://media.istockphoto.com/id/1316420668/vector/user-icon-human-person-symbol-social-profile-icon-avatar-login-sign-web-user-symbol.jpg?s=612x612&w=0&k=20&c=AhqW2ssX8EeI2IYFm6-ASQ7rfeBWfrFFV4E87SaFhJE="
+    }
   },
-  password: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-});
+  { timestamps: true }
+);
 
 // create a model
 
